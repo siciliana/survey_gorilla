bbc48b155d220ef098eefa00079305bffae5adf9
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 16:52:13 -0500

    Have edit button FUNCTIONALITY

--- app/controllers/survey_edit.rb
+++ app/controllers/survey_edit.rb
@@ -2,3 +2,18 @@ get '/survey_edit/:survey_id' do
   @survey = Survey.find(params[:survey_id])
   erb :survey_edit
 end
+
+post '/survey_edit/:survey_id' do
+  @survey = Survey.find(params[:survey_id])
+  @survey.update_attributes(title: params[:title])
+    @question = @survey.questions
+
+    puts params.inspect
+
+      @question.each do |question|
+        question.update_attributes(q_title: params[:add_question])
+      end
+  redirect to 'welcome/current_user.id'
+end
+
+

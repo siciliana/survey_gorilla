bb457c2f51e64963a86b35a058470e2caa722d30
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 13:57:46 -0500

    Added helper method and login page logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,4 +5,22 @@ end
 
 
 
-# this will contain all login/user logic
+post '/signup' do
+  user = User.create(params[:user])
+  session[:user_id] = user.id
+  redirect to '/welcome'
+end
+
+get '/welcome/:user_id' do
+  erb :welcome
+end
+
+post '/login' do
+  @user = User.find_by_username(params[:username])
+  if @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect to "/welcome/#{current_user.id}"
+  else
+    redirect to '/'
+  end
+end

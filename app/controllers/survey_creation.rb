f9a0762db559055caf4ef4f53132b3e6c562423c
Apprentice <apprentice@dbc-chicago033.local>
2013-10-19 16:35:42 -0500

    MULTIPLE QUESTIONS

--- app/controllers/survey_creation.rb
+++ app/controllers/survey_creation.rb
@@ -4,24 +4,40 @@ get '/create_survey' do
   erb :create_survey
 end
 
-post '/create_survey' do
-
-  short_link = Survey.generate_shortlink
-  @survey = Survey.create(url: ("/survey/" + "#{short_link}"), title: params[:title], user_id: 1)
-  @question = Question.create(q_type: "text", q_title: params[:add_question], survey_id: @survey.id)
-
-
-  @survey.questions << @question
-  current_user.surveys << @survey
-
-
-
-  erb :survey_success
-end
-
 get '/survey/:url' do
   @survey_url = params[:url]
 
   @survey = Survey.find_by_url("/survey/#{@survey_url}")
   erb :display_survey
 end
+
+post '/create_survey' do
+
+  short_link = Survey.generate_shortlink
+
+  @survey = Survey.find_or_create_by_title(params[:title])
+  if @survey.url == nil
+    @survey.update_attributes(url: ("/survey/" + "#{short_link}"))
+  else
+    @survey
+  end
+
+  @question = Question.create(q_type: "text", q_title: params[:add_question], survey_id: @survey.id)
+
+    if params[:question_action] == "Add another question"
+
+      @survey.questions << @question
+      @survey.save
+      current_user.surveys << @survey
+
+      redirect to '/create_survey'
+
+
+    else
+      params[:question_action] == "Submit survey"
+
+      erb :survey_success
+    end
+end
+
+

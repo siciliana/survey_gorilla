b9d80b070e6285b0786da077cc978429446c510f
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-19 18:59:10 -0500

    Can now iterate through surveys with multiple responses

--- app/controllers/survey_completion.rb
+++ app/controllers/survey_completion.rb
@@ -4,9 +4,11 @@ post "/submit_response/:survey_id" do
   @submission = Submission.create(survey_id: @survey.id)
 
   @survey.questions.each_with_index do |question,index|
-    question.responses.create(user_input: params[:response], submission_id: @submission.id)
+    question.responses.create(user_input: params[:response][index.to_s], submission_id: @submission.id)
   end
   
+  
+
 
   @survey_taker.submissions << @submission
 
---
+++ public/js/responset.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//   $('.submit_button').click(function(event){
+//     event.preventDefault();
+
+//   $('.form_input').data('form')
+//     var position = new Array();
+//       for (var i=0;i<$('.form_input').length;i++){position.push(i)};
+//     var data = {positionIndex:data};
+//     var url = $('form').attr('action');
+//   $.post(url, data, function( response ) { 
+    
+//     });
+//     });
+// });

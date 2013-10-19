e6a3af5fa02208dde82a43e8f53b714aa1369804
Apprentice <apprentice@dbc-chicago033.local>
2013-10-19 15:13:26 -0500

    properly saving user submissions

--- app/controllers/survey_completion.rb
+++ app/controllers/survey_completion.rb
@@ -1,3 +1,11 @@
 post "/submit_response/:survey_id" do
-  
+  @survey_taker = current_user
+  @survey = Survey.find(params[:survey_id])
+  @submission = Submission.create(survey_id: @survey.id)
+  @survey.questions.last.responses.create(user_input: params[:response], submission_id: @submission.id)
+  @survey_taker.submissions << @submission
+
+  redirect to "/welcome/#{current_user.id}"
+
+
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,6 @@ class Response < ActiveRecord::Base
   belongs_to :question
   belongs_to :submission
   belongs_to :choice
+  belongs_to :user
+  belongs_to :survey
 end
